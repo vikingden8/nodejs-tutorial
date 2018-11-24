75fbb1d4fceb4afaf7d2c20bf62b543236a68bbb
Viking Den <vikingden7@gmail.com>
2018-11-24 22:24:06 +0800

    write content with write stream

--- ex-10/app.js
+++ ex-10/app.js
@@ -1,9 +1,11 @@
 var http = require('http') ;
 var fs = require('fs') ;
 
-var myReadStream = fs.createReadStream('ReadContent.txt') ;
+var myReadStream = fs.createReadStream('ReadContent.txt', 'utf8') ;
+var myWriteStrem = fs.createWriteStream('WriteContent.txt', )
 
 myReadStream.on('data', function(data){
     console.log('new chunk received:');
     console.log(data.toString());
+    myWriteStrem.write(data);
 });

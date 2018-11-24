3c6f46296a2a7764d67896b7bf38cbad88189355
Viking Den <vikingden7@gmail.com>
2018-11-24 23:06:06 +0800

    update ex-10

--- ex-10/app.js
+++ ex-10/app.js
@@ -4,8 +4,12 @@ var fs = require('fs') ;
 var myReadStream = fs.createReadStream('ReadContent.txt', 'utf8') ;
 var myWriteStrem = fs.createWriteStream('WriteContent.txt', )
 
+/*
 myReadStream.on('data', function(data){
     console.log('new chunk received:');
     console.log(data.toString());
     myWriteStrem.write(data);
 });
+*/
+
+myReadStream.pipe(myWriteStrem);

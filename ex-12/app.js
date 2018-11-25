9332426379606314388e1a70a724ee3cb72276c8
Viking Den <vikingden7@gmail.com>
2018-11-25 11:13:45 +0800

    add ejs template engine

--- ex-12/app.js
+++ ex-12/app.js
@@ -2,16 +2,28 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.end('You are in Home page.');
+    // res.end('You are in Home page.');
+    res.sendFile(__dirname + '/views/home.html') ;
 });
 
 app.get('/about', function(req, res){
-    res.end('You are in About page.');
+    // res.end('You are in About page.');
+    res.sendFile(__dirname + '/views/about.html') ;
 });
 
-app.get('/profile/:id', function(req, res){
-    res.end('You requested to see the profile with the id ' + req.params.id) ;
+app.get('/profile/:name', function(req, res){
+    // res.end('You requested to see the profile with the id ' + req.params.id) ;
+    data = {
+        age: 31,
+        job: 'Develper',
+    }
+    res.render('profile', {
+        name: req.params.name,
+        data: data,
+    });
 });
 
 app.listen(3000);
